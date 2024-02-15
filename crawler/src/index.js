48976adfbd5b90b8704c15301c76bac2e4029b47
Emily L <emilythedev@gmail.com>
2024-02-15 18:02:28 +0900

    write data into csv

--- crawler/src/index.js
+++ crawler/src/index.js
@@ -1,3 +1,6 @@
+import { stringify } from 'csv';
+import { createWriteStream } from 'fs';
+import { get, map } from 'lodash-es';
 import { getItemByPageId } from './item.js';
 
 const itemList = [
@@ -5,12 +8,55 @@ const itemList = [
   { title: 'Forge', pageId: 344 },
   { title: 'Ironhead arrow', pageId: 294 },
 ];
-itemList.forEach(item => {
-  getItemByPageId(item.pageId)
-    .then(item => {
-      console.log(JSON.stringify(item));
-    })
-    .catch(error => {
-      console.error('Error:', error.message);
+
+Promise.all(
+  itemList.map(async (item) =>{
+    return await getItemByPageId(item.pageId)
+  })
+)
+  .then(write)
+  .catch(error => console.error(error));
+
+const keyMappings = [
+  ['title'],
+  ['page_id', 'pageId'],
+  ['internal_id', 'internalId'],
+  ['source'],
+  ['upgrades', 'upgrades', (list) => list.join('\n')],
+].concat([1,2,3,4].map((level, i) => {
+  return [
+    [`level_${level}_crafting_level`, `materials[${i}].craftingLevel`],
+    [`level_${level}_materials`, `materials[${i}].materials`, list => {
+      return list.map(({ quantity, title }) => {
+        return `${quantity}x ${title}`;
+      }).join('\n');
+    }],
+  ];
+}).flat());
+
+function write(list) {
+  const transformedList = list.map(item => {
+    return keyMappings.map((mappings) => {
+      if (mappings.length === 1) {
+        return get(item, mappings[0], null);
+      } else {
+        const val = get(item, mappings[1], null);
+        return mappings[2] && val ? mappings[2](val) : val;
+      }
     });
-});
+  });
+
+  const writeStream = createWriteStream('./csv/output.csv');
+
+  const stringifier = stringify({
+    header: true,
+    columns: map(keyMappings, '0'),
+  });
+
+  stringifier.pipe(writeStream);
+
+  transformedList.forEach((record) => stringifier.write(record));
+
+  stringifier.end();
+  writeStream.end();
+}
