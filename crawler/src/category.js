acacf40404d9a75a8e71442059052d2de76647fb
Emily L <emilythedev@gmail.com>
2024-02-15 20:08:42 +0900

    use generator API to get category member list

--- crawler/src/category.js
+++ crawler/src/category.js
@@ -4,24 +4,32 @@ import { apiBaseUrl } from './constants.js';
 export const getItems = async (categoryName, continueValue) => {
   const params = {
     action: 'query',
-    list: 'categorymembers',
-    cmtitle: `Category:${categoryName}`,
-    cmtype: 'page',
-    cmlimit: 20,
+    generator: 'categorymembers',
+    gcmtitle: `Category:${categoryName}`,
+    gcmtype: 'page',
+    gcmlimit: 20,
+    prop: 'info',
     format: 'json',
-    cmcontinue: continueValue || null,
+    gcmcontinue: continueValue || null,
   };
 
   const { data } = await axios.get(apiBaseUrl, { params });
+  const pages = data.query.pages;
 
-  let list = data.query.categorymembers.map(page => {
-    if (page.ns !== 0) return null; // the page is in main namespace
+  const list = [];
 
-    return {
+  for (let key in pages) {
+    const page = pages[key];
+    // skip page that is not in main namespace or is redirected
+    if (page.ns !== 0 || typeof page.redirect !== 'undefined') {
+      continue;
+    }
+
+    list.push({
       title: page.title,
       pageId: page.pageid,
-    };
-  }).filter(item => !!item);
+    });
+  }
 
   if (data.continue) {
     // continue reading data
