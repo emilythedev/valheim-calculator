90a5d1c99bfc9bad8f473eb4ef9aaa79cf9ef00a
Emily L <emilythedev@gmail.com>
2024-04-18 18:17:02 +0900

    sort filtered item list

--- valheim-calculator/src/entities/item/hooks/filter.ts
+++ valheim-calculator/src/entities/item/hooks/filter.ts
@@ -1,4 +1,5 @@
 import { useAtomValue } from 'jotai';
+import { sortBy } from 'lodash-es';
 import { useMemo } from 'react';
 import { readQueryTxtAtom, readWriteRecipesAtom } from '../atoms/recipes';
 import { filterList } from '../helpers/filter';
@@ -9,17 +10,21 @@ const useFilterList = () => {
   const list = useAtomValue(readWriteRecipesAtom);
 
   const filteredList = useMemo(() => {
+    let filtered = list;
+
     if (searchTxt.startsWith('id:')) {
-      return filterList(
+      filtered = filterList(
         list,
         'id',
         searchTxt.slice(3).split(',')
           .map(s => parseInt(s))
           .filter(i => !isNaN(i))
       );
+    } else {
+      filtered = filterList(list, 'title', searchTxt);
     }
 
-    return filterList(list, 'title', searchTxt);
+    return sortBy(list, ['title', 'qualityLevel']);
   }, [searchTxt, list]);
 
   return [filteredList, searchTxt] as const;
