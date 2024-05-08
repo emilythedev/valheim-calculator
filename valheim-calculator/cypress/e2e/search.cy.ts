3c5f2146f9daac70797844627fbf2dfc33e8bba8
Emily L <emilythedev@gmail.com>
2024-05-08 20:03:59 +0900

    test: search by exact name and search for all items contains the search text

--- valheim-calculator/cypress/e2e/search.cy.ts
+++ valheim-calculator/cypress/e2e/search.cy.ts
@@ -1,16 +1,31 @@
 /// <reference types="cypress" />
 
+const search = (text: string) => {
+  cy.get('[data-testid=cy-select-queryType').click();
+  cy.get('[data-testid=cy-option-titleType').click();
+
+  cy.get('input[name=queryInputValue]').type(text);
+  cy.get('button[type=submit]').click();
+};
+
 describe('Search', () => {
-  it('search for an item', () => {
+  beforeEach(() => {
     cy.visit('/');
+  });
 
-    cy.get('[data-testid=cy-select-queryType').click();
-    cy.get('[data-testid=cy-option-titleType').click();
+  it('search for an item by full name', () => {
+    search('"workbench"');
 
-    cy.get('input[name=queryInputValue]').type('workbench');
-    cy.get('button[type=submit]').click();
-
-    cy.get('table[data-testid=cy-table-recipes] > tbody > tr > td:first-child')
+    cy.get('table[data-testid=cy-table-recipes] > tbody > tr')
+      .should('have.length', 1)
+      .find('> td:first-child')
       .should('have.text', 'Workbench');
   });
+
+  it('search for all items containing search text', () => {
+    search('eitr');
+
+    cy.get('table[data-testid=cy-table-recipes] > tbody > tr > td:first-child')
+      .should('include.text', 'eitr');
+  })
 });
