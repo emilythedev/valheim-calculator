ed0f3469afbda15cbd36b0f004be32b8a60142bb
Emily L <emilythedev@gmail.com>
2024-05-08 23:45:52 +0900

    add test for "scroll to load"

--- valheim-calculator/cypress/e2e/search.cy.ts
+++ valheim-calculator/cypress/e2e/search.cy.ts
@@ -10,7 +10,7 @@ describe('Search', () => {
   it('search for an item by full name', () => {
     cy.search('"forge"');
 
-    cy.get('table[data-testid=cy-table-recipes] > tbody > tr')
+    cy.get('table[data-testid="cy-table-recipes"] > tbody > tr')
       .should('have.length', 1)
       .find('> td:first-child')
       .should('have.text', 'Forge');
@@ -19,7 +19,26 @@ describe('Search', () => {
   it('search for all items containing search text', () => {
     cy.search('forge');
 
-    cy.get('table[data-testid=cy-table-recipes] > tbody > tr > td:first-child')
+    cy.get('table[data-testid="cy-table-recipes"] > tbody > tr > td:first-child')
       .should('include.text', 'forge');
   });
+
+  it('scroll to load more search results', () => {
+    cy.get('[data-testid="cy-table-recipes"]').as('table');
+
+    // Get result count
+    cy.get('@table').find('tr').its('length').then((length) => {
+      // scroll to bottom
+      cy.get('@table')
+        .find('tr')
+        .last()
+        .scrollIntoView();
+
+      // compare result count
+      cy.get('@table')
+        .find('tr')
+        .its('length')
+        .should('be.gt', length);
+    });
+  })
 });
