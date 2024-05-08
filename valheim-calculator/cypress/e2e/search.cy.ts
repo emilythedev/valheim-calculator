98605d641deb13b85d45e319b46887dbf31114c1
Emily L <emilythedev@gmail.com>
2024-05-08 23:27:25 +0900

    add search as cypress command to use in other spec

--- valheim-calculator/cypress/e2e/search.cy.ts
+++ valheim-calculator/cypress/e2e/search.cy.ts
@@ -1,31 +1,25 @@
 /// <reference types="cypress" />
 
-const search = (text: string) => {
-  cy.get('[data-testid=cy-select-queryType').click();
-  cy.get('[data-testid=cy-option-titleType').click();
-
-  cy.get('input[name=queryInputValue]').type(text);
-  cy.get('button[type=submit]').click();
-};
-
 describe('Search', () => {
   beforeEach(() => {
     cy.visit('/');
+    cy.intercept('/data.json').as('jsonData');
+    cy.wait('@jsonData');
   });
 
   it('search for an item by full name', () => {
-    search('"workbench"');
+    cy.search('"forge"');
 
     cy.get('table[data-testid=cy-table-recipes] > tbody > tr')
       .should('have.length', 1)
       .find('> td:first-child')
-      .should('have.text', 'Workbench');
+      .should('have.text', 'Forge');
   });
 
   it('search for all items containing search text', () => {
-    search('eitr');
+    cy.search('forge');
 
     cy.get('table[data-testid=cy-table-recipes] > tbody > tr > td:first-child')
-      .should('include.text', 'eitr');
-  })
+      .should('include.text', 'forge');
+  });
 });
---
+++ valheim-calculator/cypress/support/commands.ts
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+declare namespace Cypress {
+  interface Chainable {
+    search(text: string): Chainable<void>
+  }
+}
+
+Cypress.Commands.add('search', (text) => {
+  cy.get('[data-testid=cy-select-queryType').click();
+  cy.get('[data-testid=cy-option-titleType').click();
+
+  cy.get('input[name=queryInputValue]').type(text);
+  cy.get('button[type=submit]').click();
+});
