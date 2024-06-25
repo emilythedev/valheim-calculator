69a622c5ab69556210738ce716de4c50a446c342
Emily L <emilythedev@gmail.com>
2024-06-25 18:21:53 +0900

    remove all components from page and add finder

---
+++ valheim-calculator/src/features/recipes/Finder.tsx
@@ -0,0 +1,126 @@
+import { getCraftableEntityList, getEntityMaxQuality } from '@/data';
+import { Button } from '@/shared/ui/button';
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList
+} from '@/shared/ui/command';
+import { Popover, PopoverContent, PopoverTrigger } from '@/shared/ui/popover';
+import { Search, Star } from 'lucide-react';
+import { useState } from 'react';
+
+const entityList = getCraftableEntityList();
+
+interface FilterSearchProps {
+  value: EntityId,
+  onValueChange: (value: EntityId) => void,
+}
+
+const FilterSearch = ({ value, onValueChange }: FilterSearchProps) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          size="lg"
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+        >
+          {value ? value : 'Search an item'}
+          <Search className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="p-0" align="start">
+        <Command
+          className="rounded-lg border shadow-md"
+        >
+          <CommandInput placeholder="Type item name..." />
+          <CommandList>
+            <CommandEmpty>No results found.</CommandEmpty>
+            <CommandGroup>
+              {
+                entityList.map(({ key, label }) => (
+                  <CommandItem
+                    key={key}
+                    value={key}
+                    onSelect={(val) => {
+                      onValueChange(val);
+                      setOpen(false);
+                    }}
+                  >{ label }</CommandItem>
+                ))
+              }
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+interface QualityButtonProps {
+  quality: number,
+  onClick: (quality: number) => void,
+}
+
+const QualityButton = ({ quality, onClick }: QualityButtonProps) => {
+  return (
+    <Button variant="outline" onClick={() => onClick(quality)}>
+      <Star className="mr-1 h-4 w-4" />
+      <span className="mr-2">{quality}</span>
+      +1
+    </Button>
+  )
+};
+
+interface QualityListProps {
+  value: EntityId,
+  onAdd : (quality: number) => void,
+}
+
+const QualityList = ({ value, onAdd }: QualityListProps) => {
+  const maxQuality = getEntityMaxQuality(value);
+  const children = [];
+  for (let i = 0; i < maxQuality; i++) {
+    children.push((
+      <QualityButton
+        key={i}
+        quality={i + 1}
+        onClick={onAdd}
+      />
+    ))
+  }
+
+  return (
+    <>
+      {children}
+    </>
+  );
+};
+
+interface FinderProps {
+  onRecipeSelect?: (id: EntityId, quality: number) => void,
+}
+
+const Finder = ({ onRecipeSelect }: FinderProps) => {
+  const [value, setValue] = useState<EntityId>('');
+  const handleAdd = (quality: number) => {
+    onRecipeSelect && onRecipeSelect(value, quality);
+  }
+
+  return (
+    <>
+      <FilterSearch value={value} onValueChange={setValue} />
+      <div className="flex flex-row justify-stretch gap-2">
+        <QualityList value={value} onAdd={handleAdd} />
+      </div>
+    </>
+  );
+};
+
+export default Finder;
--- valheim-calculator/src/pages/Home.tsx
+++ valheim-calculator/src/pages/Home.tsx
@@ -1,29 +1,12 @@
-import SearchSection from '@/widgets/SearchSection';
-import SummarySection from '@/widgets/SummarySection';
-import { Box, Container, Stack } from '@mui/joy';
-
+import RecipeFinder from '@/features/recipes/Finder';
 
 const Layout = () => {
   return (
-    <Container sx={{
-      height: '100vh',
-      display: 'flex',
-    }}>
-      <Stack
-        flexGrow={1}
-        alignItems="stretch"
-        spacing={4}
-        my={4}
-        direction="row"
-      >
-        <Box display="flex" flex="0 0 50%">
-          <SearchSection />
-        </Box>
-        <Box display="flex" flex="0 0 50%">
-          <SummarySection />
-        </Box>
-      </Stack>
-    </Container>
+    <div className="container min-h-screen flex flex-col">
+      <div className="flex-1 flex flex-col justify-center align-center">
+        <RecipeFinder />
+      </div>
+    </div>
   );
 };
 
