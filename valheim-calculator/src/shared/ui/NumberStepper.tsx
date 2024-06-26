544d8b9df28d4b6338a348724c6dd8a5d2559735
Emily L <emilythedev@gmail.com>
2024-06-26 22:14:29 +0900

    create NumberStepper from StepperInput with shadcn-ui

---
+++ valheim-calculator/src/shared/ui/NumberStepper.tsx
@@ -0,0 +1,53 @@
+import { Minus, Plus } from 'lucide-react';
+import { Button } from './button';
+import { Input } from './input';
+
+interface Props {
+  value: number,
+  onChange?: (value: number) => void,
+  step?: number,
+  min?: number,
+  max?: number,
+}
+
+const NumberStepper = ({step = 1, min = 0, max = Number.POSITIVE_INFINITY, ...props}: Props) => {
+  const onChange = (value: number) => {
+    if (!props.onChange || isNaN(value)) return;
+
+    value = Math.max(Math.min(value, max), min);
+    if (value !== props.value) {
+      props.onChange(value);
+    }
+  };
+
+  return (
+    <div className="inline-flex flex-row">
+      <Button
+        data-testid="cy-stepper-dec"
+        variant="secondary"
+        size="icon"
+        onClick={() => onChange(props.value - step)}
+        className="rounded-r-none"
+      >
+        <Minus className="h-4 w-4" />
+      </Button>
+      <Input
+        data-testid="cy-stepper"
+        value={props.value}
+        onChange={(e) => onChange(parseInt(e.target.value))}
+        className="text-center w-12 rounded-none border-x-0"
+      />
+      <Button
+        data-testid="cy-stepper-inc"
+        variant="default"
+        size="icon"
+        onClick={() => onChange(props.value + step)}
+        className="rounded-l-none"
+      >
+        <Plus className="h-4 w-4" />
+      </Button>
+    </div>
+  )
+};
+
+export default NumberStepper;
