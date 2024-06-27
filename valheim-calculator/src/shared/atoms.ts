f5c6e721e246cb0393436c92c88bb4522251995c
Emily L <emilythedev@gmail.com>
2024-06-27 18:53:05 +0900

    cache path name

--- valheim-calculator/src/shared/atoms.ts
+++ valheim-calculator/src/shared/atoms.ts
@@ -17,28 +17,30 @@ const shelfAtom = atomWithStorage(
 
 export const readRecipesAtom = atom(get => get(recipesAtom));
 
-const getRecipeKeyPath = ({ entity, quality }: RecipeKey) => `${entity}|${quality}`;
+const getPath = ({ entity, quality }: RecipeKey) => `${entity}|${quality}`;
 
 export const recipeAmountAtoms = atomFamily(
-  (recipe: RecipeKey) => atom(
-    (get) => _get(get(shelfAtom), getRecipeKeyPath(recipe)) || 0,
-    (_, set, amount: number) => {
-      set(shelfAtom, (shelf) => {
-        const path = getRecipeKeyPath(recipe);
-        const newValue = omit(shelf, [path]);
-        if (amount > 0) {
-          _set(newValue, path, amount);
-        }
-        return newValue;
-      });
-      set(recipesAtom, (list) => {
-        const i = findIndex(list, (r) => r.entity === recipe.entity && r.quality === recipe.quality);
-        if (i > -1) {
-          return amount > 0 ? list : [...list.slice(0, i), ...list.slice(i + 1)];
-        }
-        return amount > 0 ? [...list, recipe] : list;
-      });
-    },
-  ),
+  (recipe: RecipeKey) => {
+    const path = getPath(recipe);
+    return atom(
+      (get) => _get(get(shelfAtom), path, 0),
+      (_, set, amount: number) => {
+        set(shelfAtom, (shelf) => {
+          const newValue = omit(shelf, [path]);
+          if (amount > 0) {
+            _set(newValue, path, amount);
+          }
+          return newValue;
+        });
+        set(recipesAtom, (list) => {
+          const i = findIndex(list, (r) => r.entity === recipe.entity && r.quality === recipe.quality);
+          if (i > -1) {
+            return amount > 0 ? list : [...list.slice(0, i), ...list.slice(i + 1)];
+          }
+          return amount > 0 ? [...list, recipe] : list;
+        });
+      },
+    );
+  },
   (a, b) => (a.entity === b.entity && a.quality === b.quality)
 );
