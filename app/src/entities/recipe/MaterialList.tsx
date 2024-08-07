49c8e9f9db9df0f6ab265f55c637cc509e128bc0
Emily L <emilythedev@gmail.com>
2024-08-07 22:39:38 +0900

    update material list layout on small screens

--- app/src/entities/recipe/MaterialList.tsx
+++ app/src/entities/recipe/MaterialList.tsx
@@ -1,5 +1,6 @@
 import { getEntityName, isEntityCraftable } from '@/data';
-import { EntityContextProvider, useEntityContext } from '@/entities/entity/provider';
+import EntityName from '@/entities/entity/EntityName';
+import { EntityContextProvider } from '@/entities/entity/provider';
 import AmountControl from '@/entities/recipe/AmountControl';
 import { RecipeContextProvider } from '@/entities/recipe/provider';
 import KeyValueList from '@/shared/ui/KeyValueList';
@@ -18,16 +19,11 @@ const NonCraftableItem = () => {
   );
 };
 
-const EntityName = () => {
-  const { name } = useEntityContext();
-  return (<span>{ name }</span>);
-};
-
 const CraftableItem = () => {
   return (
     <RecipeContextProvider quality={1}>
-      <div className="entity-list-item justify-between bg-secondary text-secondary-foreground">
-        <EntityName />
+      <div className="entity-list-item relative justify-between bg-secondary text-secondary-foreground flex-wrap">
+        <EntityName className="max-sm:w-full" />
         <AmountControl />
       </div>
     </RecipeContextProvider>
@@ -38,7 +34,7 @@ const ListItem = ({ entity, amount }: ListItemProps) => {
   const craftable = isEntityCraftable(entity);
 
   return (
-    <div role="listitem" className="grid grid-cols-subgrid col-span-2 items-center">
+    <div role="listitem" className="grid grid-cols-subgrid col-span-2 items-baseline">
       <div className="text-center px-2 py-2">{ amount }</div>
       <EntityContextProvider entity={entity}>
         { craftable ? <CraftableItem /> : <NonCraftableItem /> }
@@ -55,7 +51,7 @@ const MaterialList = ({ materials }: { materials: RecipeMaterials }) => {
   return (
     <KeyValueList
       list={materials}
-      className="text-sm grid grid-cols-form gap-2 items-center"
+      className="text-sm grid grid-cols-form gap-2 items-baseline"
       item={(entity, amount: number) => (
         <ListItem key={entity} entity={entity} amount={amount} />
       )}
